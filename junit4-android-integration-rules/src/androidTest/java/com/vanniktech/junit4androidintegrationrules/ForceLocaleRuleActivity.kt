e4ae8071f21ae5c669956dbb0efaf49f35bbc066
Niklas Baudy <niklas.baudy@vanniktech.de>
2019-02-25 13:36:08 +0100

    Update dependencies to latest versions. (#40)
    * Update dependencies to latest versions.
    
    * Update Travis.
    
    * Install tools.

--- junit4-android-integration-rules/src/androidTest/java/com/vanniktech/junit4androidintegrationrules/ForceLocaleRuleActivity.kt
+++ junit4-android-integration-rules/src/androidTest/java/com/vanniktech/junit4androidintegrationrules/ForceLocaleRuleActivity.kt
@@ -1,5 +1,6 @@
 package com.vanniktech.junit4androidintegrationrules
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
@@ -8,7 +9,7 @@ import java.util.Locale
 class ForceLocaleRuleActivity : Activity() {
   lateinit var textView: TextView
 
-  override fun onCreate(savedInstanceState: Bundle?) {
+  @SuppressLint("ResourceType") override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     textView = TextView(this).apply {
