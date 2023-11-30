6bb7e29575061922103b8202700a07ac5e1712ab
Niklas Baudy <niklas.baudy@vanniktech.de>
2023-11-29 21:04:58 -0300

    Changes.

--- junit4-android-integration-rules/src/main/java/com/vanniktech/junit4androidintegrationrules/BarsCommand.kt
+++ junit4-android-integration-rules/src/main/java/com/vanniktech/junit4androidintegrationrules/BarsCommand.kt
@@ -2,7 +2,9 @@ package com.vanniktech.junit4androidintegrationrules
 
 /** Allows you to modify Bars as per https://android.googlesource.com/platform/frameworks/base/+/master/packages/SystemUI/docs/demo_mode.md */
 class BarsCommand private constructor() : Command("bars") {
-  fun mode(mode: BarMode) = apply { map["mode"] = mode; }
+  fun mode(mode: BarMode) = apply {
+    map["mode"] = mode
+  }
 
   enum class BarMode(override val value: String) : EnumValue {
     BAR_MODE_OPAQUE("opaque"),
